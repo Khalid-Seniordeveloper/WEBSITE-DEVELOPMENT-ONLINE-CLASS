2ffef233709e916e7c4fb17a589cd3d318af2570
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-10-03 12:16:45 +0500

    Javascript startedf

---
+++ JAVASCRIPT/Class 1/app.js
@@ -0,0 +1,23 @@
+// 3 TYPES VALUES 
+
+// String (condition => "1234")
+// Number  (ANY NUMBER => 1234)
+// BOOLEAN (trur or false)
+
+
+// let person Name = "MUHAMMAD KHALID";
+
+// console.log("hello ! my name is" + " " + personName)  ;
+
+
+// let num1 = 1234;
+// console.log(num1);
+
+
+// const Rose = "flower";
+// console.log(Rose);
+
+// alert("welcome!");
+
+// let userName = prompt("what's your name ?");
+// console.log("User name is here" + " " +userName);
