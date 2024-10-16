b357990cb1032cb09b52d9913e292af07a845710
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-10-16 23:52:03 +0500

    done

---
+++ JAVASCRIPT/Class 2/app.js
@@ -0,0 +1,72 @@
+// alert('Welcome !')
+// let userName = prompt("Enter your username here");
+// let age = prompt('enter your age here');
+// let Education = prompt('Enter your education here');
+
+// console.log("Your name " + " " + userName + ";");
+// console.log("Your Age" + " " + age);
+// console.log("Your Education" + " " + Education);
+
+// let num1 = 7;
+// let num2 = 3 ;
+// let num3 = 10
+// let total = (num1 - num2) * num3 % ;
+// console.log(2 % 2);
+
+
+// let num1 = prompt("Enter first number here");
+
+// let num2 = prompt('Enter second number here');
+// let num3 = prompt('Enter Third number here');
+// let num4 = prompt('Enter Fourrth number here');
+
+
+// console.log(  " Total : " + (num1 + num2 + num3 + num4));
+
+
+// let var1 = 2;
+// let var2 = "2";
+
+// console.log(var1 + var2);
+
+// console.log("my name is" + "Khalid");
+
+// var username = " khalid";
+// console.log(username);
+
+// console.log(typeof 22);
+// alert(2 + 2)
+// console.log(10 % 3);
+
+// console.log(42 % 5);
+
+
+// conditional statement 
+
+
+// let day = "2";
+
+// if(day === 2 ){
+//     console.log("SOJA SUNDAY hy");
+    
+// } else {
+//     console.log("KAAM PE JA");
+    
+// }
+
+
+// === is means type will also check
+
+// == its mean only values are check
+
+
+// let var1 = prompt("enter first number here");
+// let var2 = prompt('Enter second number here');
+
+// if (var1 + var2 == 22){
+//     console.log("HAN BHAI 22 HY");
+    
+// } else {
+//     console.log("NH HY");
+    
+// }
---
+++ JAVASCRIPT/Class 3/app.js
@@ -0,0 +1,32 @@
+// console.log("HELLO" + " " + "JS");
+// console.log(5 +  3);
+
+// alert("alert here !")
+// let variable = prompt("enter your name here?");
+
+// console.log(variable);
+
+// if else statement 
+// conditional statement
+
+// if true ////// falase ? ellse 
+
+
+// let num1 = '22';
+
+// if(num1 === 22){
+//  alert("22 is present");
+// } else {
+//     console.log("22 is absent!");
+    
+// }
+
+let num = prompt("enter your number here divisible by 3" );
+
+if(num % 3 === 0){
+    console.log("number is divisble by 3");
+    
+} else {
+    console.log("nikal");
+    
+}
